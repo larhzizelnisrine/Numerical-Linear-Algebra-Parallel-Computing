08eb523834f068cb6755bf1f938cacdfe6a4154d
larhzizelnisrine <76496712+larhzizelnisrine@users.noreply.github.com>
2023-04-13 00:19:19 +0000

    Add files via upload

---
+++ mpi-week/assignments/exo1.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 13 00:16:56 2023
+
+@author: nisri
+"""
+
+from mpi4py import MPI
+
+# Initialize MPI and get the rank and size of the current process
+comm = MPI.COMM_WORLD
+rank = comm.Get_rank()
+size = comm.Get_size()
+
+#Qst 1 and 2                                                               
+print("Hello World! I'm rank", rank);
+if rank==0:
+    print("Total no. of ranks =", size);
+
+# Qst 3
+if rank==0 :
+    print("I have the right to print the rank", rank);
